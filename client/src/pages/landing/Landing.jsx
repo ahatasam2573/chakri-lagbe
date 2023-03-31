050ed588531d78a5b75af5466c85500dbacde678
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-03-31 16:50:05 +0600

    landing first components completd

--- client/src/pages/landing/Landing.jsx
+++ client/src/pages/landing/Landing.jsx
@@ -1,10 +1,11 @@
 import logo from '../../assets/logo.png'
 import main from '../../assets/main.svg';
+import styled from 'styled-components'
 import './Landing.css'
 
 const Landing = () => {
     return (
-        <main>
+        <Wrapper>
             <nav className='nav-content'>
                 <img src={logo} alt="chakri-lagbe" className='logo' />
                 <h1>Chakri Lagbe</h1>
@@ -18,12 +19,38 @@ const Landing = () => {
                 </div>
                 <img src={main} alt="chakri" className='img main-img' />
             </div>
-        </main>
+        </Wrapper>
     )
 }
 
 const Wrapper = styled.main`
-
+   .page{
+    min-height: calc(70vh);
+    display: grid;
+    align-items: center;
+   }
+   h1{
+    font-weight: 700;
+   }
+   span{
+    color: var(--primary-500);
+   }
+   p{
+    color: var(--grey-600);
+   }
+   .main-img{
+    display: none;
+   }
+   @media(min-width: 992px){
+    .page{
+        grid-template-columns: 1fr 1fr;
+        column-gap: 3rem;
+    }
+    .main-img{
+        display: block;
+    }
+   }
 `
+
 
 export default Landing
