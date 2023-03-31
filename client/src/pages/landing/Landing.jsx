804cb72536577b53ed443755ba220eb6f8edaf6b
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-03-31 16:28:20 +0600

    all are in client

--- client/src/pages/landing/Landing.jsx
+++ client/src/pages/landing/Landing.jsx
@@ -9,8 +9,21 @@ const Landing = () => {
                 <img src={logo} alt="chakri-lagbe" className='logo' />
                 <h1>Chakri Lagbe</h1>
             </nav>
+
+            <div className="container page">
+                <div className="info">
+                    <h1>job <span>Tracking</span> app</h1>
+                    <p>Welcome to your dream jobs where you can find yours jobs according to your skill. Just explore and find your jobs to fulfil your dream.</p>
+                    <button className='btn btn-hero'>Login/Register</button>
+                </div>
+                <img src={main} alt="chakri" className='img main-img' />
+            </div>
         </main>
     )
 }
+
+const Wrapper = styled.main`
+
+`
 
 export default Landing
