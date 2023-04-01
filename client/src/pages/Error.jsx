86a585abee0ca6363fc3717884d057d996b4399f
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-04-01 23:02:10 +0600

    Error page completed

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom"
+import Wrapper from "../assets/wrapper/ErrorPage"
+import ErrorImage from '../assets/img/not-found.svg'
+
 const Error = () => {
     return (
-        <div>Error</div>
+        <Wrapper className="full-page">
+            <div>
+                <img src={ErrorImage} alt="Error404" />
+                <h3>Sorry! Page Not Found</h3>
+                <p>We can't find any page that you're looking for</p>
+                <Link to='/'>Back home</Link>
+            </div>
+        </Wrapper>
     )
 }
 export default Error
