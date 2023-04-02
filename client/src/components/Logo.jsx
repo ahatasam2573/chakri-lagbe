c50c85a78b0b939d769f2c221f778d3e2f02d742
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-04-02 21:57:24 +0600

    Logo component cretaed

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import logo from '../assets/img/logo.png'
+
+const Logo = () => {
+    return <img src={logo} alt='chakri-lagbe-logo' className='logo' />
+}
+
+export default Logo;
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,13 +1,13 @@
 import Main from '../assets/img/main.svg';
 import Wrapper from '../assets/wrapper/LandingPage';
-import logo from '../assets/img/logo.png'
 import { Link } from 'react-router-dom';
+import Logo from '../components/Logo';
 
 const Landing = () => {
     return (
         <Wrapper>
             <nav className='nav-content'>
-                <img src={logo} alt='jobify' className='logo' />
+                <Logo />
                 <h1>Chakri Lagbe</h1>
             </nav>
 
